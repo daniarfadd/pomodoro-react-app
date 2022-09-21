63821d27eda25bb6ed409035bdf7a1c8c3f9fad0
daniarfadd <daniarfadaffa@yahoo.com>
2022-09-21 14:43:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className="timer">
+        <span id="hour"> 00 </span>
+        :
+        <span id="minute"> 00 </span>
+        :
+        <span id="second"> 00 </span>
+        :
+        <span id="millisecond"> 000 </span>
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
